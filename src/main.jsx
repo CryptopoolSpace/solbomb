9563d0f209b18b3d2480e69c3f496739d6d5106c
git-chef <37466523+CryptopoolSpace@users.noreply.github.com>
2026-06-14 00:21:04 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom'
+import '@solana/wallet-adapter-react-ui/styles.css'
+
+const wallets = [new PhantomWalletAdapter()]
+const endpoint = 'https://api.devnet.solana.com'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <App />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   </React.StrictMode>
 )
