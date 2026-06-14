d8a9e66b67beda0f0a3e0c6738f6530e845eac65
git-chef <37466523+CryptopoolSpace@users.noreply.github.com>
2026-06-14 14:08:30 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,32 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {},
+    global: 'globalThis',
+  },
+  resolve: {
+    alias: {
+      stream: 'stream-browserify',
+      buffer: 'buffer',
+    },
+  },
+  optimizeDeps: {
+    include: [
+      '@coral-xyz/anchor',
+      '@solana/web3.js',
+      '@solana/spl-token',
+      'buffer',
+    ],
+    esbuildOptions: {
+      target: 'esnext',
+    },
+  },
+  build: {
+    target: 'esnext',
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
 })
+
