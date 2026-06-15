3b79ce458aeeb0087e3fed0d7adb461717ab791d
git-chef <37466523+CryptopoolSpace@users.noreply.github.com>
2026-06-15 15:57:13 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': {},
-    global: 'globalThis',
-  },
+  'process.env': {},
+  global: 'globalThis',
+  'Buffer': ['buffer', 'Buffer'],
+},
+
   resolve: {
     alias: {
       stream: 'stream-browserify',
